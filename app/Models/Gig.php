1a89c9ce690ec918eb686eadb02cc5c623948ddc
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-07 10:55:23 +0100

    adding the relationship on gigs model

--- app/Models/Gig.php
+++ app/Models/Gig.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Gig extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'Role_id',
+        'company_id',
+        'country_id',
+        'state_id',
+        'address',
+        'min',
+        'max',
+    ];
+
+
+    /**
+     * adding the relationship with Tag table
+     * a gig has many tags
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags(){
+
+        return $this->belongsToMany(Tag::class);
+
+    }
+
+
+    /**
+     * a gig belongs to role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role(){
+
+        return  $this->belongsTo(Role::class);
+    }
 }
