bca5eb25d43746ae54a87559654647a39d51dd9b
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-07 15:21:24 +0100

    company blade created

--- resources/views/partial/_admin_sidebar.blade.php
+++ resources/views/partial/_admin_sidebar.blade.php
@@ -7,7 +7,7 @@
                     <a href="" data-toggle="collapse" data-target="#manage-job" aria-expanded="true" aria-controls="manage-job"><i class="lni lni-add-files mr-2"></i>Gigs</a>
 
                     <ul id="manage-job" class="collapse">
-                        <li><a href="{{ route('gigs.index') }}"><i class="lni lni-files mr-2"></i>View All Gigs<span class="count-tag bg-warning">4</span></a></li>
+                        <li><a href="{{ route('gigs.index') }}"><i class="lni lni-files mr-2"></i>View All Gigs</a></li>
                         <li><a href="{{ route('gigs.create') }}"><i class="lni lni-add-files mr-2"></i>Add a New Gig </a></li>
                     </ul>
 
@@ -16,8 +16,8 @@
                 <li class="accordion">
                     <a href="" data-toggle="collapse" data-target="#manage-user" aria-expanded="true" aria-controls="manage-user"><i class="lni lni-add-files mr-2"></i>Company</a>
                     <ul id="manage-user" class="collapse">
-                        <li><a href="#"><i class="lni lni-bookmark mr-2"></i>View All Users<span class="count-tag bg-warning">4</span></a></li>
-                        <li><a href="#"><i class="lni lni-mastercard mr-2"></i>Add New User</a></li>
+                        <li><a href="#"><i class="lni lni-bookmark mr-2"></i>View All Company</a></li>
+                        <li><a href="#"><i class="lni lni-mastercard mr-2"></i>Add New Company</a></li>
 
                     </ul>
 
