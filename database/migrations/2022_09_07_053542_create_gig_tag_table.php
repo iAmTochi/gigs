13c11a5e5321c285542b6811a79211540ddc2e2f
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-07 07:51:10 +0100

    Update 2022_09_07_053542_create_gig_tag_table.php

--- database/migrations/2022_09_07_053542_create_gig_tag_table.php
+++ database/migrations/2022_09_07_053542_create_gig_tag_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('gig_id');
             $table->unsignedBigInteger('tag_id');
-            $table->timestamps();
         });
     }
 
