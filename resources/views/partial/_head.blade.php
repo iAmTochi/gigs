41231e739648b0f5aa3d635f80339c2dd83619ee
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-09-07 15:18:10 +0100

    removed

--- resources/views/partial/_head.blade.php
+++ resources/views/partial/_head.blade.php
@@ -2,9 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8" />
-    <meta name="author" content="Dynaton Digital Solutions" />
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ asset('favicon.ico') }}"/>
 
     <title>{{ config('app.name', 'You are Good') }} </title>
 
